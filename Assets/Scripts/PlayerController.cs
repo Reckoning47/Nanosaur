7237417f8faeea268bea7f5ecb28c662e2df3a4b
Reckoning47 <day0fdiligence@gmail.com>
2020-11-15 21:46:56 -0700

    terrain

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody playerBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement()
+    {
+        if (Input.GetAxis("Vertical"))
+        {
+
+        }
+        playerBody.AddRelativeForce(Vector3.up);
+    }
+}
