ea0dfbb9ff8e6563cdee9e29c23e6c2757562580
Reckoning47 <day0fdiligence@gmail.com>
2021-11-14 15:08:07 -0700

    Struggling with animations. Not important at the moment. TODO enemies!

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -17,8 +17,12 @@ public class ThirdPersonShooterController : MonoBehaviour
     [SerializeField]
     private LayerMask aimColliderLayerMask = new LayerMask();
 
-    [SerializeField]
-    private Transform debugTransform;
+    [SerializeField] private Transform debugTransform;
+    [SerializeField] private Transform bulletProjectile;
+    [SerializeField] private Transform spawnBulletPos;
+
+    [SerializeField] private Transform vfxHitGreen;
+    [SerializeField] private Transform vfxHitRed;
 
     private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
@@ -31,24 +35,65 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     private void Update()
     {
-        if(starterAssetsInputs.aim)
+        Vector3 mouseWorldPos = Vector3.zero;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2f, Screen.height / 2f));
+        Transform hitScanTransform = null;
+
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+
+            mouseWorldPos = raycastHit.point;
+
+            hitScanTransform = raycastHit.transform;
+
+        }
+
+        if (starterAssetsInputs.aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
             thirdPersonController.SetSensitivity(aimSensitivity);
+            thirdPersonController.SetRotateOnMove(false);
+
+            Vector3 worldAim = mouseWorldPos;
+            worldAim.y = transform.position.y;
+            Vector3 aimDirection = (worldAim - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+
         }
         else
         {
             aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.SetSensitivity(normalSensitivity);
+            thirdPersonController.SetRotateOnMove(true);
         }
-        //(Screen.width /2f, Screen.width /2f);
 
-        //Vector2 screenCenterpoint
 
-        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2f, Screen.height / 2f));
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        if (starterAssetsInputs.shoot)
         {
-            debugTransform.position = raycastHit.point;
+            // hitscan shooting
+            //if(hitScanTransform != null)
+            //{
+            //    if (hitTransform.GetComponent<BulletTarget>() != null)
+            //    {
+            //        // Hit target
+            //        Instantiate(vfxHitGreen, raycastHit.point, Quaternion.identity);
+            //    }
+            //    else
+            //    {
+            //        // Hit something else
+            //        Instantiate(vfxHitRed, raycastHit.point, Quaternion.identity);
+            //    }
+            //}
+
+            // projectile based shooting
+            Vector3 aimDir = (mouseWorldPos - spawnBulletPos.position).normalized;
+            Instantiate(bulletProjectile, spawnBulletPos.position, Quaternion.LookRotation(aimDir, Vector3.up));
+
+            starterAssetsInputs.shoot = false;
+
         }
 
     }
